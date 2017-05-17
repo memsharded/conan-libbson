c2d73da6e06023e9594c9e4f9490edd80343be3e
theirix <theirix@gmail.com>
2017-05-17 11:40:48 +0300

    Upgrade to AutoToolsBuildEnvironment, version 1.6.2

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,10 @@
-from conans import ConanFile, ConfigureEnvironment
-from conans.tools import download, untargz, check_sha1, replace_in_file
+from conans import ConanFile, AutoToolsBuildEnvironment
+from conans.tools import download, untargz, check_sha1, replace_in_file, environment_append
 import os
-import shutil
 
 class LibbsonConan(ConanFile):
     name = "libbson"
-    version = "1.6.0"
+    version = "1.6.2"
     url = "https://github.com/theirix/conan-libbson"
     license = "https://github.com/mongodb/libbson/blob/master/COPYING"
     description = "A BSON utility library."
@@ -21,17 +20,15 @@ class LibbsonConan(ConanFile):
         tarball_name = self.FOLDER_NAME + '.tar.gz'
         download("https://github.com/mongodb/libbson/releases/download/%s/%s.tar.gz"
                  % (self.version, self.FOLDER_NAME), tarball_name)
-        check_sha1(tarball_name, "c56f4ba0b164b959210b362ce7b533f34f28faa3")
+        check_sha1(tarball_name, "67e5e290eded1f9d130b476964bc99b5fc15dfb2")
         untargz(tarball_name)
         os.unlink(tarball_name)
 
     def build(self):
 
-        env = ConfigureEnvironment(self.deps_cpp_info, self.settings)
-
         if self.settings.os == "Linux" or self.settings.os == "Macos":
 
-            env_line = env.command_line
+            env_build = AutoToolsBuildEnvironment(self)
 
             # compose configure options
             suffix = ''
@@ -40,22 +37,23 @@ class LibbsonConan(ConanFile):
             else:
                 suffix += " --disable-shared --enable-static"
 
-            # refresh configure
-            cmd = 'cd %s/%s && %s autoreconf --force --verbose --install -I build/autotools' % (self.conanfile_directory, self.FOLDER_NAME, env_line)
-            self.run(cmd)
+            with environment_append(env_build.vars):
+                # refresh configure
+                cmd = 'cd %s/%s && autoreconf --force --verbose --install -I build/autotools' % (self.conanfile_directory, self.FOLDER_NAME)
+                self.run(cmd)
 
-            # disable rpath build
-            old_str = "-install_name \$rpath/"
-            new_str = "-install_name "
-            replace_in_file("%s/%s/configure" % (self.conanfile_directory, self.FOLDER_NAME), old_str, new_str)
+                # disable rpath build
+                old_str = "-install_name \$rpath/"
+                new_str = "-install_name "
+                replace_in_file("%s/%s/configure" % (self.conanfile_directory, self.FOLDER_NAME), old_str, new_str)
 
-            cmd = 'cd %s/%s && %s ./configure %s' % (self.conanfile_directory, self.FOLDER_NAME, env_line, suffix)
-            self.output.warn('Running: ' + cmd)
-            self.run(cmd)
+                cmd = 'cd %s/%s && ./configure %s' % (self.conanfile_directory, self.FOLDER_NAME, suffix)
+                self.output.warn('Running: ' + cmd)
+                self.run(cmd)
 
-            cmd = 'cd %s/%s && %s make' % (self.conanfile_directory, self.FOLDER_NAME, env_line)
-            self.output.warn('Running: ' + cmd)
-            self.run(cmd)
+                cmd = 'cd %s/%s && make' % (self.conanfile_directory, self.FOLDER_NAME)
+                self.output.warn('Running: ' + cmd)
+                self.run(cmd)
 
     def package(self):
         # exclude private headers
