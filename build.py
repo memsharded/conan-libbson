df4d99e63553a795176d8306983c496ccf777b22
theirix <theirix@gmail.com>
2018-01-16 23:12:05 +0300

    Use testing channel

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="theirix", channel="ci")
+    builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="libbson:shared", pure_c=True)
     builder.run()
